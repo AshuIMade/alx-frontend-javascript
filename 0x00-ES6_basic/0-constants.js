0dd69459b177fcd0f016ef262c24158e3b2a7dcd
Ashenafi Yakob <asheimade@gmail.com>
2023-07-24 22:24:01 +0300

    modied using let and const new

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -6,7 +6,7 @@ export function getLast() {
     return ' is okay';
 }
 export function taskNext() {
-    let combination = 'but sometimes let';
+    let combination = 'But sometimes let';
     combination += getLast();
     return combination;
 }
